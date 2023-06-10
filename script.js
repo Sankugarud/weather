5025ed60ede586cb8868799f22a9db0873d269a9
sanket <95356246+Sankugarud@users.noreply.github.com>
2023-06-10 17:26:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,53 +14,59 @@ btn.addEventListener('click', () =>{
 });
 
 async function apicall(){
-    console.log(search.value);
-    let api = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search.value}&units=metric&appid=${apiKey}`);
-    let responce = await api.json();
-    console.log(responce.main.temp);
 
+    try {
+            console.log(search.value);
+        let api = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search.value}&units=metric&appid=${apiKey}`);
+        let responce = await api.json();
+        console.log(responce.main.temp);
+
+        
     
-   
-    let images = document.getElementById("images");
-    if(responce.weather[0].main == "Clouds"){
-        console.log(responce.weather.main);
-        images.src = "./images/clouds.png";
-    }else if(responce.weather[0].main == "Clear"){
-        images.src = "./images/clear.png";
-        console.log(responce.weather.main);
-    }else if(responce.weather[0].main == "Drizzle"){
-        images.src = "./images/drizzle.png";
-        console.log(responce.weather.main);
-    }else if(responce.weather[0].main == "mist"){
-        images.src = "./images/mist.png";
-        console.log(responce.weather.main);
-    }else if(responce.weather[0].main == "Rain"){
-        images.src = "./images/rain.png";
-        console.log(responce.weather.main);
-    }else if(responce.weather[0].main == "Snow"){
-        images.src = "./images/snow.png";
-        console.log(responce.weather.main);
-    }else if(responce.weather[0].main == "Haze"){
-        images.src = "./images/haze1.png";
-        console.log(responce.weather.main);
-    }else if(responce.weather[0].main == "Smoke"){
-        images.src = "./images/smoke.png";
-        console.log(responce.weather.main);
+        let images = document.getElementById("images");
+        if(responce.weather[0].main == "Clouds"){
+            console.log(responce.weather.main);
+            images.src = "./images/clouds.png";
+        }else if(responce.weather[0].main == "Clear"){
+            images.src = "./images/clear.png";
+            console.log(responce.weather.main);
+        }else if(responce.weather[0].main == "Drizzle"){
+            images.src = "./images/drizzle.png";
+            console.log(responce.weather.main);
+        }else if(responce.weather[0].main == "mist"){
+            images.src = "./images/mist.png";
+            console.log(responce.weather.main);
+        }else if(responce.weather[0].main == "Rain"){
+            images.src = "./images/rain.png";
+            console.log(responce.weather.main);
+        }else if(responce.weather[0].main == "Snow"){
+            images.src = "./images/snow.png";
+            console.log(responce.weather.main);
+        }else if(responce.weather[0].main == "Haze"){
+            images.src = "./images/haze1.png";
+            console.log(responce.weather.main);
+        }else if(responce.weather[0].main == "Smoke"){
+            images.src = "./images/smoke.png";
+            console.log(responce.weather.main);
+        }
+        
+        let name = document.getElementById("name");
+        name.innerHTML = responce.name;
+        let temprature = document.getElementById("temp");
+        temprature.innerHTML = responce.main.temp +" °C";
+        let humadity = document.getElementById("humadity");
+        humadity.innerHTML = responce.main.humidity + " %";
+        let speed = document.getElementById("speed");
+        speed.innerHTML = responce.wind.speed + " km/h"
+
+        let sc = document.getElementById("second-part");
+        let container = document.getElementById("container");
+        sc.style.display = "block";
+        container.style.height = "500px";
+
+        search.value = "";
+    } catch (error) {
+        alert("Plz Enter Correct City Name!!!");
     }
     
-    let name = document.getElementById("name");
-    name.innerHTML = responce.name;
-    let temprature = document.getElementById("temp");
-    temprature.innerHTML = responce.main.temp +" °C";
-    let humadity = document.getElementById("humadity");
-    humadity.innerHTML = responce.main.humidity + " %";
-    let speed = document.getElementById("speed");
-    speed.innerHTML = responce.wind.speed + " km/h"
-
-    let sc = document.getElementById("second-part");
-    let container = document.getElementById("container");
-    sc.style.display = "block";
-    container.style.height = "500px";
-
-    search.value = "";
 };  
